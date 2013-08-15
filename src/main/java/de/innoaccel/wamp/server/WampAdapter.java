d1aa50194c463589aae4e800d99380005656411c
Fritz Gerneth <fgerneth@effms.de>
2013-08-15 22:11:07 +0200

    Make socket store injectible

--- src/main/java/de/innoaccel/wamp/server/WampAdapter.java
+++ src/main/java/de/innoaccel/wamp/server/WampAdapter.java
@@ -10,26 +10,25 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.adapter.TextWebSocketHandlerAdapter;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 public class WampAdapter extends TextWebSocketHandlerAdapter
 {
 
-    private final Map<String, Websocket> sessionMap;
+    private final Map<String, Websocket> socketStore;
 
     private final Converter messageConverter;
 
-    public WampAdapter(Converter messageConverter)
+    public WampAdapter(Converter messageConverter, Map<String, Websocket> socketStore)
     {
-        this.sessionMap = new HashMap<String, Websocket>();
+        this.socketStore = socketStore;
         this.messageConverter = messageConverter;
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage rawMessage) throws InvalidMessageCode, MessageParseError, IOException
     {
-        Websocket socket = this.sessionMap.get(session.getId());
+        Websocket socket = this.socketStore.get(session.getId());
         Message message = socket.deserializeMessage(rawMessage.getPayload());
     }
 
@@ -37,14 +36,14 @@ public class WampAdapter extends TextWebSocketHandlerAdapter
     public void afterConnectionEstablished(WebSocketSession session) throws InvalidMessageCode, IOException
     {
         Websocket socket = new Websocket(session, this.messageConverter);
-
-        this.sessionMap.put(session.getId(), socket);
+        String sessionId = session.getId();
+        this.socketStore.put(sessionId, socket);
         socket.sendMessage(new WelcomeMessage());
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws InvalidMessageCode, IOException
     {
-        this.sessionMap.remove(session.getId());
+        this.socketStore.remove(session.getId());
     }
 }
--- src/test/java/de/innoaccel/wamp/server/WampAdapterTest.java
+++ src/test/java/de/innoaccel/wamp/server/WampAdapterTest.java
@@ -4,13 +4,15 @@ import de.innoaccel.wamp.server.converter.Converter;
 import de.innoaccel.wamp.server.converter.InvalidMessageCode;
 import de.innoaccel.wamp.server.message.Message;
 import de.innoaccel.wamp.server.message.WelcomeMessage;
+import junit.framework.Assert;
 import mockit.*;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.util.Assert;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class WampAdapterTest
 {
@@ -19,26 +21,43 @@ public class WampAdapterTest
     @Mocked
     private Converter messageConverter;
 
+    @Mocked
+    private Map<String, Websocket> socketStore;
+
     @Before
     public void setUp()
     {
-        this.adapter = new WampAdapter(this.messageConverter);
+        this.adapter = new WampAdapter(this.messageConverter, this.socketStore);
+    }
+
+    @Test
+    public void afterConnectionEstablishedSavesNewAdapterInStore(final WebSocketSession session)
+        throws InvalidMessageCode, IOException
+    {
+        new NonStrictExpectations() {{
+            WampAdapterTest.this.messageConverter.serialize((Message) any, (Websocket) any); result = "serialized";
+            session.getId(); result = "sessionId";
+        }};
+
+        this.adapter.afterConnectionEstablished(session);
+
+        new Verifications() {{
+            WampAdapterTest.this.socketStore.put("sessionId", (Websocket) any);
+        }};
     }
 
     @Test
     public void sendsWelcomeMessageWithSessionIdAfterEstablishedConnection(final WebSocketSession session) throws InvalidMessageCode, IOException
     {
-        new MockUp<Websocket>() {
-            @Mock
-            public void sendMessage(Message message)
-            {
-                Assert.isInstanceOf(WelcomeMessage.class, message);
-            }
-        };
         new NonStrictExpectations() {{
             session.getId(); result = "sessionId";
+            WampAdapterTest.this.messageConverter.serialize((Message) any, (Websocket) any); result = "serialized";
         }};
 
         this.adapter.afterConnectionEstablished(session);
+
+        new Verifications() {{
+            session.sendMessage((TextMessage) any);
+        }};
     }
 }
