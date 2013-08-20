63f2cbfe5cbaddc79092904792e6fe1d0b680d6c
Fritz Gerneth <fgerneth@effms.de>
2013-08-20 13:57:09 +0200

    Remove unnecessary message wrapper of exception

--- src/main/java/de/innoaccel/wamp/server/converter/MessageParseException.java
+++ src/main/java/de/innoaccel/wamp/server/converter/MessageParseException.java
@@ -2,17 +2,4 @@ package de.innoaccel.wamp.server.converter;
 
 public class MessageParseException extends Exception
 {
-    private String message;
-
-    public MessageParseException(String message)
-    {
-        super("Cannot parse message");
-
-        this.message = message;
-    }
-
-    public String getRawMessage()
-    {
-        return this.message;
-    }
 }
