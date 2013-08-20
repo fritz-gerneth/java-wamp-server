c36768ceedf8bff41a7555b8ce211dd11835547c
Fritz Gerneth <fgerneth@effms.de>
2013-08-20 22:06:25 +0200

    Move much of the json processing into abstract base class

--- src/main/java/de/innoaccel/wamp/server/converter/EventMessageConverter.java
+++ src/main/java/de/innoaccel/wamp/server/converter/EventMessageConverter.java
@@ -3,11 +3,9 @@ package de.innoaccel.wamp.server.converter;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.JsonNodeType;
 import de.innoaccel.wamp.server.Websocket;
 import de.innoaccel.wamp.server.message.EventMessage;
 import de.innoaccel.wamp.server.message.Message;
-import java.io.IOException;
 
 public class EventMessageConverter extends JsonParsingConverter<EventMessage>
 {
@@ -41,48 +39,16 @@ public class EventMessageConverter extends JsonParsingConverter<EventMessage>
         }
     }
 
-    @Override
-    public EventMessage deserialize(String message, Websocket socket) throws MessageParseException, InvalidMessageCodeException
+    protected EventMessage deserialize(JsonNode rootNode, Websocket socket) throws MessageParseException, InvalidMessageCodeException
     {
-        JsonNode rootNode;
-        try {
-            rootNode = this.objectMapper.readTree(message);
-        } catch (IOException parseException) {
-            // TODO: check when this actually can happen, build a test-case for this
-            throw new MessageParseException(parseException.getMessage());
-        }
+        this.assertMessageCode(rootNode, Message.EVENT);
 
-        if (JsonNodeType.ARRAY != rootNode.getNodeType()) {
-            throw new MessageParseException("Message has invalid markup (not an array)");
-        }
-
-        JsonNode messageCodeNode = rootNode.get(0);
-        if (null == messageCodeNode || JsonNodeType.NUMBER != messageCodeNode.getNodeType() || !messageCodeNode.canConvertToInt()) {
-            throw new MessageParseException("Message has invalid markup (message code not a nunber)");
-        }
-
-        int messageCode = messageCodeNode.asInt();
-        if (Message.EVENT != messageCode) {
-            throw new InvalidMessageCodeException(messageCode);
-        }
-
-        JsonNode topicNode = rootNode.get(1);
-        if (null == topicNode || JsonNodeType.STRING != topicNode.getNodeType()) {
-            throw new MessageParseException("Message has invalid markup (topic is no string)");
-        }
-
-        String topic = topicNode.asText();
+        String topic = this.readStringAt(rootNode, 1);
         if (0 == topic.length()) {
             throw new MessageParseException("Message has invalid markup (topic is not valid)");
         }
-
-        JsonNode payloadNode = rootNode.get(2);
-        if (null == payloadNode) {
-            throw new MessageParseException("Message has invalid markup (no payload)");
-        }
-
         // TODO: can we formulate further contraints for the payload? Provide tests about the content?
 
-        return new EventMessage(socket.inflateCURI(topic), payloadNode);
+        return new EventMessage(socket.inflateCURI(topic), this.readAnyTypeAt(rootNode, 2));
     }
 }
